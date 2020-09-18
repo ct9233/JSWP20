cd6086cd116f28e961c3d6b0729c151a645e710e
Chris Twaddell <ct9233@gmail.com>
2020-09-17 17:29:28 -0700

    Infinite Scroll Script

--- 02 - Infinite Scroll/script.js
+++ 02 - Infinite Scroll/script.js
@@ -48,4 +48,11 @@ async function getPhotos() {
     }
 }
 
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+        getPhotos();
+    }
+});
+
 getPhotos();
