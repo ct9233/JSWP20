174a06d1423c1347e921dd6a290c47d19b84501e
Chris Twaddell <ct9233@gmail.com>
2020-10-28 17:04:48 -0700

    Score Page

--- 15 - Math Sprint Game/script.js
+++ 15 - Math Sprint Game/script.js
@@ -40,9 +40,43 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY = 0;
 
+// Reset Game
+function playAgain() {
+	gamePage.addEventListener('click', startTimer);
+	scorePage.hidden = true;
+	splashPage.hidden = false;
+	equationsArray = [];
+	playerGuessArray = [];
+	valueY = 0;
+	playAgainBtn.hidden = true;
+}
+
+// Show Score Page
+function showScorePage() {
+	// Show Play Again button after 1 second
+	setTimeout(() => {
+		playAgainBtn.hidden = false
+	}, 1000);
+	gamePage.hidden = true;
+	scorePage.hidden = false;
+}
+
+// Format & Display Time in DOM
+function scoresToDOM() {
+	finalTimeDisplay = finalTime.toFixed(1);
+	baseTime = timePlayed.toFixed(1);
+	penaltyTime = penaltyTime.toFixed(1);
+	baseTimeEl.textContent = `Base Time: ${baseTime}s`;
+	penaltyTimeEl.textContent = `Penalty: +${penaltyTime}s`;
+	finalTimeEl.textContent = `${finalTimeDisplay}s`;
+	// Scroll to Top, go to Score Page
+	itemContainer.scrollTo({ top: 0, behavior: 'instant' });
+	showScorePage();
+}
+
 // Stop Timer, Process Results, go to Score Page
 function checkTime() {
-	if (playerGuessArray.length === questionAmount) {
+	if (playerGuessArray.length == questionAmount) {
 		clearInterval(timer);
 		// Check for wrong guesses, add penalty time
 		equationsArray.forEach((equation, index) => {
@@ -54,6 +88,7 @@ function checkTime() {
 			}
 		});
 		finalTime = timePlayed + penaltyTime;
+		scoresToDOM();
 	}
 }
 
